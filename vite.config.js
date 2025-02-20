ca4258a9befadcfd7e1e57f388c822b2f90d6766
amalkrishna0123 <amalikka0@gmail.com>
2025-02-20 16:46:34 +0530

    Second Commit

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build',
+  },
   plugins: [
-    react(), tailwindcss()
+    react(), tailwindcss(),
   ],
 })
